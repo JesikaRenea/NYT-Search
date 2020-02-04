5ff5fad50bc9f4707f20292079860248a892165a
JesikaRenea <JessicaHayesUCF@outlook.com>
2020-02-03 21:35:45 -0500

    Added variables to select input values

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-var searchTerm = "nas"
-var numResults = 5;
-var beginDate = 2000;
-var endDate = 2005;
+$("#run-search").on("click", function (event) {
+event.preventDefault();
 
-var queryUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=sdxIGIb5cqZt4ppvxFHXI2jWK2JhAnAF';
+    var searchTerm = $("#search-term").val().trim();
+    var numResults = $("#num-records").val();
+    var beginDate = $("#start-year").val();
+    var endDate = $("#end-year").val();
 
-queryUrl += "&q=" + searchTerm;
+    var queryUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=sdxIGIb5cqZt4ppvxFHXI2jWK2JhAnAF';
 
-queryUrl += '&begin_date=' + beginDate + "0101";
+    queryUrl += "&q=" + searchTerm;
+    queryUrl += "&page=" + numResults;
+    queryUrl += '&begin_date=' + beginDate + "0101";
+    queryUrl += "&end_date=" + endDate + "0101";
 
-queryUrl +=  "&end_date=" + endDate + "0101";
 
+    $.ajax({
+        url: queryUrl,
+        method: 'GET'
+    })
+        .then(function (response) {
+            console.log(response)
+        });
 
-$.ajax({
-url: queryUrl,
-method: 'GET'
-})
-
-.then(function(response){
-
-  console.log(response)
-})
+});
